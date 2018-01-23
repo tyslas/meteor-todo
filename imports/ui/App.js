cbe6330ece38f8003e8ccd0c8f9feb86b405f24b
Tito Yslas <roberto.yslas@gmail.com>
2018-01-23 10:33:59 -0700

    initial commit

---
+++ imports/ui/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks';
+
+import Task from './Task';
+
+// App component represents the root of the whole app
+class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  renderTasks() {
+    return this.props.tasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this) } >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
+        </header>
+
+        <ul>{this.renderTasks()}</ul>
+      </div>
+    );
+  }
+}
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+  };
+})(App);
---
+++ imports/ui/Task.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import { Tasks } from '../api/tasks';
+
+// task component - represents a single todo item in the list
+export default class Task extends Component {
+  toggleChecked() {
+      // set the checked property to the opposite of its current value
+      Tasks.update(this.props.task._id, {
+        $set: { checked: !this.props.task.checked },
+      });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
+  render() {
+    // give tasks a different className when they are checked off
+    // this allows us to use CSS to style them
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+
+    return (
+      <li className={taskClassName}>
+        <button
+          className="delete"
+          onClick={this.deleteThisTask.bind(this)}
+        >&times;</button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={!!this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
+    );
+  }
+}
