67225e1889ec3bcba590f2ab1cec409356f682f8
Tito Yslas <roberto.yslas@gmail.com>
2018-01-23 14:54:52 -0700

    added feature to show the count of uncompleted tasks

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -46,7 +46,7 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
 
           <label className="hide-completed">
             <input
@@ -75,5 +75,6 @@ class App extends Component {
 export default withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 })(App);
