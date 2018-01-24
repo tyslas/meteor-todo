52de0ce5fe7d85431cc6e3fa9df52e73438c89f7
Tito Yslas <roberto.yslas@gmail.com>
2018-01-24 12:51:07 -0700

    added mocha to do a unit test to confirm that the delete method works

---
+++ imports/api/tasks.test.js
@@ -0,0 +1,40 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'chai';
+
+import { Tasks } from './tasks';
+
+if (Meteor.isServer) {
+  describe('Tasks', () => {
+    describe('methods', () => {
+      const userId = Random.id();
+      let taskId;
+
+      beforeEach(() => {
+        Tasks.remove({});
+        taskId = Tasks.insert({
+          text: 'test task',
+          createdAt: new Date(),
+          owner: userId,
+          username: 'Kenny'
+        });
+      });
+
+      it('can delete owned task', () => {
+        // find the internal implementation of the task method so that we can test it in isolation
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+        // set up a fake method invocation that looks like what the method expects
+        const invocation = { userId };
+
+        // run the method with 'this' set to the fake invocation
+        deleteTask.apply(invocation, [taskId]);
+
+        // verify that the method does what we expected
+        assert.equal(Tasks.find().count(), 0);
+      });
+    });
+  });
+}
